ee702fc3bd6d10ac1cee5155def5b30cbf2f27d4
Jack Scotti <jacopojackscotti@gmail.com>
2016-10-23 17:47:37 +0100

    Add QueryBuilder

---
+++ src/main/java/app/QueryBuilder.java
@@ -0,0 +1,54 @@
+package app;
+
+import com.esotericsoftware.yamlbeans.YamlReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class QueryBuilder {
+
+    final String ROOT_URL;
+    final String SEARCH_PARAMS_PATH;
+    final String KEY_PATH;
+
+    public QueryBuilder(){
+        ROOT_URL = "http://api.zoopla.co.uk/api/v1/property_listings.json?";
+        SEARCH_PARAMS_PATH = "./src/main/resources/search_parameters.yml";
+        KEY_PATH = "./src/main/resources/.secrets.yml";
+    }
+
+    public String build() throws IOException {
+
+        String query = ROOT_URL;
+
+        Iterator<HashMap.Entry<String, String>> entries = params().entrySet().iterator();
+        while (entries.hasNext()) {
+            HashMap.Entry<String, String> entry = entries.next();
+            query += entry.getKey();
+            query += "=";
+            query += entry.getValue();
+            query += "&";
+        }
+
+        return query;
+    }
+
+    private HashMap params() throws IOException {
+        YamlReader reader = new YamlReader(new FileReader(SEARCH_PARAMS_PATH));
+        HashMap<String, String> searchParams = (HashMap) reader.read();
+
+        reader = new YamlReader(new FileReader(KEY_PATH));
+        HashMap<String, String> key = (HashMap) reader.read();
+
+        reader.close();
+
+        HashMap<String, String> allParams = new HashMap<>();
+
+        allParams.putAll(searchParams);
+        allParams.putAll(key);
+
+        return allParams;
+    }
+}
